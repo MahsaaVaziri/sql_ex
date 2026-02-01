efc049a4cd67ce673f935b96f46a5f00d7e7068e
MahsaaVaziri <mahsav72.mv@gmail.com>
2026-02-01 20:18:46 +0330

    the first commit

---
+++ Student.sql
@@ -0,0 +1,34 @@
+-- Write a query that finds students who do not take CS180.
+
+SELECT s.student_no,student_name,e.course_no,course_title 
+FROM students s,student_enrollment e, courses c
+where s.student_no = e.student_no
+AND e.course_no = c.course_no
+AND e.course_no != 'CS180'
+
+-- Write a query to find students who take CS110 or CS107 but not both.
+
+SELECT s.student_no,student_name,e.course_no,course_title 
+FROM students s,student_enrollment e, courses c
+where s.student_no = e.student_no
+AND e.course_no = c.course_no
+AND e.course_no IN ('CS107','CS110')
+
+-- Write a query to find students who take CS220 and no other courses.
+
+SELECT s.student_no,student_name,e.course_no,course_title 
+FROM students s,student_enrollment e, courses c
+where s.student_no = e.student_no
+AND e.course_no = c.course_no
+AND e.course_no = 'CS220'
+
+-- Write a query that finds those students who take at 
+-- most 2 courses. Your query should exclude students that 
+-- don't take any courses as well as those that take more 
+-- than 2 course.
+SELECT s.student_no,student_name,e.course_no,course_title 
+FROM students s,student_enrollment e, courses c
+where s.student_no = e.student_no
+AND e.course_no = c.course_no
+-- Write a query to find students who are older than at most two other students.
+SELECT * FROM students 
