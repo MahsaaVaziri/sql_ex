c89b918ec44ce52b8b435d287114620f30fdcd73
MahsaaVaziri <mahsav72.mv@gmail.com>
2026-02-03 18:33:37 +0330

    the last commit

--- Student.sql
+++ Student.sql
@@ -26,9 +26,23 @@ AND e.course_no = 'CS220'
 -- most 2 courses. Your query should exclude students that 
 -- don't take any courses as well as those that take more 
 -- than 2 course.
-SELECT s.student_no,student_name,e.course_no,course_title 
-FROM students s,student_enrollment e, courses c
-where s.student_no = e.student_no
-AND e.course_no = c.course_no
+
+SELECT s.student_no, s.student_name, s.age
+
+FROM students s, student_enrollment se
+
+WHERE s.student_no = se.student_no
+
+GROUP BY s.student_no, s.student_name, s.age
+
+HAVING COUNT(*) <= 2
 -- Write a query to find students who are older than at most two other students.
-SELECT * FROM students 
+SELECT s1.*
+
+FROM students s1
+
+WHERE 2 >= (SELECT count(*)
+
+                FROM students s2
+
+                WHERE s2.age < s1.age)
